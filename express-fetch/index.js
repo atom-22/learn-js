e47a72574fef1b89fd3e28eea744d36a93ff2f3f
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-12 18:53:20 +0400

    get forked repos from github

--- express-fetch/index.js
+++ express-fetch/index.js
@@ -3,11 +3,11 @@ var app = express();
 var path = require('path');
 var fetch = require('isomorphic-fetch');
 
-app.get('/user', (req, res) => { 
-    res.sendFile(path.resolve(__dirname, "user.json"));  
-});
+// app.get('/user', (req, res) => { 
+//     res.sendFile(path.resolve(__dirname, "user.json"));  
+// });
 
-app.listen(5000, ()=>(console.log("Port 5000")));
+// app.listen(5000, ()=>(console.log("Port 5000")));
 
 
 // 1.geted user data and loged it using fetch API
@@ -23,12 +23,36 @@ app.listen(5000, ()=>(console.log("Port 5000")));
 
 // 2.geted local user data from our express server and from other server
 
-var localJson = fetch('http://localhost:5000/user');
-var globalJson = fetch('https://jsonplaceholder.typicode.com/posts/1');
+// var localJson = fetch('http://localhost:5000/user');
+// var globalJson = fetch('https://jsonplaceholder.typicode.com/posts/1');
 
-Promise.all([localJson,globalJson])
-  .then(result => console.log("Result!"),
-        error => console.log("Error!") 
-)
+// Promise.all([localJson,globalJson])
+//   .then(result => console.log("Result!"),
+//         error => console.log("Error!") 
+// )
+
+
+//3.using fetch get forked repos from github
+
+app.get('/', (req, res) => {
+    res.send(req.query.username);                             // /?username = ''
+});
+
+app.listen(3000, ()=>console.log('Port 3000'));
+
+
+userdata = fetch('https://api.github.com/users/atom-22/repos');
+
+userdata
+    .then(response => response.json())
+    .then((json)=>{
+        for(let i=0;i < json.length;i++){
+            if(json[i].fork === true){
+                console.log(json[i].id);
+            }       
+        }  
+    })
+    .catch(e => console.error(e));
+
 
 
