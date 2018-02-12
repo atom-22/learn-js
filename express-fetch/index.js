0c3dfd7ab3cf5965948db527302f04037fccecb7
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-12 18:51:08 +0400

    data from our express server and from other server

--- express-fetch/index.js
+++ express-fetch/index.js
@@ -7,13 +7,28 @@ app.get('/user', (req, res) => {
     res.sendFile(path.resolve(__dirname, "user.json"));  
 });
 
-app.listen(5000);
+app.listen(5000, ()=>(console.log("Port 5000")));
 
-var requestJson = fetch('http://localhost:5000/user');
 
-requestJson
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(e => console.log(e));
+// 1.geted user data and loged it using fetch API
+
+// var localJson = fetch('http://localhost:5000/user');
+
+// localJson
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+//   .catch(e => console.log(e));
+
+
+
+// 2.geted local user data from our express server and from other server
+
+var localJson = fetch('http://localhost:5000/user');
+var globalJson = fetch('https://jsonplaceholder.typicode.com/posts/1');
+
+Promise.all([localJson,globalJson])
+  .then(result => console.log("Result!"),
+        error => console.log("Error!") 
+)
 
 
