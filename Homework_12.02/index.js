bc94ff1c920b4505ee7049b67423e0b2d9f5641d
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-15 16:45:24 +0400

    changed func names, added new function

--- Homework_12.02/index.js
+++ Homework_12.02/index.js
@@ -1,32 +1,34 @@
 var arr = [53,45,7,-12,6,3];
 
-function logArrCopy(arr){
+function copyArr(arr){
 	var copyArr = [];
 	arr.map(function(x){
 		copyArr.push(x);
 	});
-	console.log(copyArr);	
+	return copyArr;	
 }
 
+console.log(copyArr(arr));
 
-logArrCopy(arr);
 
-
-function lagCopyArr(arr){
+function arrCopy(arr){
 	var copyArr = [];
 	arr.forEach(function(x){
 		copyArr.push(x);
 	});
-	console.log(copyArr);	
+	return copyArr;	
 }
 
-
-lagCopyArr(arr);
-
+console.log(arrCopy(arr));
 
 
-var sum = [1, 2, 3].reduce(function (a, b) {
-  return a + b;
-}, 10);
 
-console.log(sum);
+var revArr = [1,2,3,5].reduce(function (a, b, index, array){
+	var inArr = [];
+	for(var i = index; i >= array[0]; --i){
+		inArr.push(array[i]);
+	}
+  	return inArr;
+},0);
+
+console.log(revArr);
