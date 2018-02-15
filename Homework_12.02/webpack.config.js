83e902aae75b7aa3cf2c619f7b7859b9523fa206
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-16 00:29:59 +0400

    added package json version to output filename

--- Homework_12.02/webpack.config.js
+++ Homework_12.02/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const packageData = require('./package.json');
 
 module.exports = {
 	entry: "./src/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.[hash].js"
+		filename: `bundle.[hash].${packageData.version}.js`
 	}
 };
