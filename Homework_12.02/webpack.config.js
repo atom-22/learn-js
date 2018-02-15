62bbedbeda190538312e231e1e65c724d5645752
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-15 18:25:22 +0400

    added hashed filename

--- Homework_12.02/webpack.config.js
+++ Homework_12.02/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-	entry : "./src/index.js",
-	output : {
+	entry: "./src/index.js",
+	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js"
+		filename: "build.[hash].js"
 	}
 };
