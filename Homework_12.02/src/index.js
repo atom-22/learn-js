90e20987844452e95642f4ed5cebd43b94a998b2
Atom Ulikhanyan <atomulikhanyan@gmail.com>
2018-02-16 14:19:39 +0400

    Added deep copy of array using lodash and for loop

--- Homework_12.02/src/index.js
+++ Homework_12.02/src/index.js
@@ -1,8 +1,12 @@
-var pjson = require('../package.json');
-console.log(pjson.scripts);
+const _ = require('lodash');
 
 const array = [53,45,7,-12,6,3];
 
+// Array deep copy using lodash 
+const deepCopy = _.map(array, _.clone);
+console.log(deepCopy);
+
+//Array shallow copy using map and array's push method
 function copyArray(arr){
 	let copyArr = [];
 	arr.map(function(x){
@@ -12,15 +16,30 @@ function copyArray(arr){
 }
 console.log(copyArray(array));
 
+//Array copy
 function arrayCopy(arr){
 	let copyArr = [];
 	arr.forEach(function(x){
-		copyArr.push(x);
+		let i = 0;
+		i = x;
+		copyArr.push(i);
+		++i;
 	});
 	return copyArr;	
 }
 console.log(arrayCopy(array));
 
+//Array deep copy using for loop
+function arrayDeepCopy(){
+	let arr = [];
+	for(let i=0, length = array.length; i<length; i++){
+		arr[i] = array[i];
+	}
+	return arr;
+}
+console.log(arrayDeepCopy());
+
+//reversed array using reduce
 const reverseArray = [1,2,3,5].reduce(function (a, b, index, array){
 	let innerArray = [];
 	for(let i = index; i >= array[0]; --i){
